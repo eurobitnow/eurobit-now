e03668ecebe7a578b51f7aadcd47ccb0d155d4a6
Codex02 <32749284+Codex02@users.noreply.github.com>
2017-11-24 22:17:57 -0500

    genesis

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "Eurobit";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ece9d9b6c137029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210143ace73cc2a10040f50c104a29eba08e0b9ffdd317f53ab92fed257ff88d74dc";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "010501ff0001ece9d9b6c137029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101cad2dadf94f3d3021aaa601452ed9ab476116bc68944df3474af6158d068d5e7";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
